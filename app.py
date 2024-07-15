d53f8c55d57c44bcdd4a7d03d0e25aeb61aecade
Akbar <fitrahaliakbar@gmail.com>
2024-07-15 07:40:51 +0700

    lil update again

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ if selected == "Predict":
 if selected == "About":
     st.title("Tentang Pembuat AI")
     st.write("""
-        Aplikasi ini dibuat oleh Fitrah Ali Akbar Setiawan (https://github.com/KOCOKERZ) dan Megah Juliardi Sondara Wicaksana (https://github.com/juliardimegah) yang memiliki minat besar dalam pengembangan AI. Anda dapat melihat proyek-proyek lainnya di GitHub saya.
+        Aplikasi ini dibuat oleh Fitrah Ali Akbar Setiawan (https://github.com/KOCOKERZ) dan Megah Juliardi Sondara Wicaksana (https://github.com/juliardimegah) yang memiliki minat besar dalam pengembangan AI. Anda dapat melihat proyek-proyek lainnya di GitHub kami.
     """)
     st.markdown("Github Fitrah Ali Akbar Setiawan [![GitHub](https://img.shields.io/badge/GitHub-000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/KOCOKERZ)")
     st.markdown("Github Megah Juliardi Sondara Wicaksana [![GitHub](https://img.shields.io/badge/GitHub-000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/juliardimegah)")
